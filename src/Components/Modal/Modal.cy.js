f61e5323699d71c6383c1739a48e862370f0dfbb
sultanrayhan32 <sultanrayhanh@gmail.com>
2023-02-09 17:47:56 +0700

    Add modal unit test

---
+++ src/Components/Modal/Modal.cy.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Modal from "./Modal";
+
+describe('<Modal />',()=>{
+
+    it('renders' , ()=>{
+        cy.mount(<Modal/>)   
+    })
+
+    it('The title Modal text should be "Create Task" if the status is create new title modal',()=>{
+        cy.mount(<Modal/>)
+        cy.get('[data-cy="create-task-modal"]').should('have.text','Create Task')
+    })
+
+    it('The title Modal text should be "Edit Task" if the status is edit new title modal',()=>{
+        cy.mount(<Modal/>)
+        cy.get('[data-cy="edit-task-modal"]').should('have.text','Edit Task')
+    })
+
+    it('The title Modal text should be "Delete Task" if the status is delete new title modal',()=>{
+        cy.mount(<Modal/>)
+        cy.get('[data-cy="delete-task-modal"]').should('have.text','Create Task')
+    })
+
+    it('The edit task input should have default value',()=>{
+        cy.get('[data-cy="input-edit-task-name]"').should('not.empty')
+    })
+
+    it('The create task input should have placeholder',()=>{
+        cy.get('[data-cy="input-create-task-name]"').should('have.attr', 'placeholder', 'Type your task')
+        cy.get('[data-cy="input-create-task-progress]"').should('have.attr', 'placeholder', 'Type your progress')
+    })
+
+})
