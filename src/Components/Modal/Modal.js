466cefbe120ae4cbe0e45e7563300d220be92bbd
sultanrayhan32 <sultanrayhanh@gmail.com>
2023-02-11 23:57:10 +0700

    Add modal create task and the other(css) for reusable

--- src/Components/Modal/Modal.js
+++ src/Components/Modal/Modal.js
@@ -4,7 +4,6 @@ import './Modal.css';
 
 function Modal ({id}) {
 
-
     let handleModalClick = (e) => {
         const modal = document.getElementById(`modal${id}`)
         if (e.target===modal) {
@@ -28,6 +27,35 @@ function Modal ({id}) {
                         style={{color:"#404040"}}
                     />   
                 </div>
+                <form>
+                    <div className="input-content">
+                        <span>
+                            Task Name
+                        </span>
+                        <input placeholder="Type your task"/>
+                    </div>
+                    <div 
+                        className="input-content" 
+                        style={{marginTop:16}}
+                    >
+                        <span>
+                            Proggress
+                        </span>
+                        <input 
+                            placeholder="70%" 
+                            type={"number"}
+                            style={{width:143}}
+                        />
+                    </div>
+                </form>
+                <div className="button-container">
+                    <button className="b2">
+                        Cancel
+                    </button>
+                    <button className="b1">
+                        Save Task
+                    </button>
+                </div>
             </div>
         </div>
     )
@@ -35,4 +63,3 @@ function Modal ({id}) {
 }
 
 export default Modal;
-
