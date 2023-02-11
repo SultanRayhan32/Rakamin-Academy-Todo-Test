db20f4f4381aea8f7a90cef360f61b5d8102853f
sultanrayhan32 <sultanrayhanh@gmail.com>
2023-02-12 01:22:28 +0700

    Add integration testing for actions API

--- src/App.cy.js
+++ src/App.cy.js
@@ -8,39 +8,59 @@ describe('API ACTION' ,()=>{
         cy.mount(<App/>)
     })
 
-    it("Create new task API",()=>{
+    it('All of the actions API should be worked properly',()=>{
+        let todosId = 0
         cy.request({
             method: 'POST', 
-            url: `${URL}todos/1/items`, 
+            url: `${URL}todos`, 
             body : {
-                name: "TEST TEST",
-                progress_percentage : 90
+                title: "Testing Data",
+                description : "June - Desember"
             },
             headers: {
                 Authorization: API
             }
         }).then((data)=>{
-            cy.mount(<App/>)
-            // cy.log(cy.get('[data-cy="board-container"]').children())
-            // cy.get('[data-cy="board-container"]').children().should('have.length', data.body.length)
+            cy.log(data[0])
+            todosId = data.body.id
+            return cy.request({
+                method: 'POST', 
+                url: `${URL}todos/${data.body.id}/items`, 
+                body : {
+                    name: "First Item",
+                    progress_percentage: 60
+                },
+                headers: {
+                    Authorization: API
+                }
+            })
         })
-    })
-
-
-    it("Update task API",()=>{
-        cy.request({
-            method: 'PATCH', 
-            url: `${URL}todos/1/items/2`, 
-            body : {
-                name: "Change Name Test"
-            },
-            headers: {
-                Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjo5LCJleHAiOjE2ODQ1NTgzNjd9.k_krTkxTdx_j8LjT03BPT-bKlecQFJs5yYXri6aHZVc'
-            }
-        }).then((data)=>{
-            cy.mount(<App/>)
-            // cy.log(cy.get('[data-cy="board-container"]').children())
-            // cy.get('[data-cy="board-container"]').children().should('have.length', data.body.length)
+        .then((data)=>{
+            cy.log(data)
+            return cy.request({
+                method: 'PATCH', 
+                url: `${URL}todos/${todosId}/items/${data.body.id}`, 
+                body : {
+                    target_todo_id : todosId,
+                    name: "First Item has change",
+                },
+                headers: {
+                    Authorization: API
+                }
+            })
+        })
+        .then((data)=>{
+            cy.log(data)
+            return cy.request({
+                method: 'DELETE', 
+                url: `${URL}todos/${todosId}/items/${data.body.id}`, 
+                headers: {
+                    Authorization: API
+                }
+            })
+        })
+        .then((data)=>{
+            cy.log("THE API HAS BEEN WORKING PROPERLY")
         })
     })
 })
